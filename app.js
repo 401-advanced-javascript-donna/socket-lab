b03e78b4f968fd47ac807258b69eec8db6ffb6c5
sanvean74 <dlambertpdx@gmail.com>
2019-10-09 16:41:19 -0700

    travis!

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:7890')
+const socket = io.connect('http://localhost:7890');
 
 socket.on('hello', () => {
   console.log('HI!');
