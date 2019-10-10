317585024aac0a92be8c25419ceb7476295b9688
sanvean74 <dlambertpdx@gmail.com>
2019-10-09 17:12:58 -0700

    writer js working!

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ io.on('connection', socket => {
   socket.on('capitalize', data => {
     console.log(data);
   });
-
+  socket.on('file-write', data => {
+    console.log(data);
+  });
+  socket.on('file-save', data => {
+    console.log(data);
+  });
   socket.on('file-error', err => {
     console.log(err);
   });
--- writer.js
+++ writer.js
@@ -1,6 +1,15 @@
-// const io = require('socket.io-client');
-// const socket = io.connect('http://localhost:7890');
-// const fs = require('fs').promises;
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:7890');
+const fileWrite = require('./file-write');
+
+fileWrite('./new-text.txt', 'did gyre and gimble in all the wabe')
+  .then(() => {
+    socket.on('file-write', data => {
+      socket.emit('file-save', data);
+    });
+  });
+
+// need to do error still
 
 
 // write a file
