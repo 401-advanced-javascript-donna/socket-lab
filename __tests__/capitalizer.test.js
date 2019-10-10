3e2106afa60190feea40de70732bf28b678a95e9
sanvean74 <dlambertpdx@gmail.com>
2019-10-09 22:30:43 -0700

    mock test passing

--- __tests__/capitalizer.test.js
+++ __tests__/capitalizer.test.js
@@ -1,9 +1,30 @@
-const capitalizer = require('../capitalizer');
+jest.mock('../file-read', () => {
+  return () => Promise.resolve('Some mock text');
+});
 
-describe('Capitalizer test', () => {
-  it('capitalizes a string', () => {
-    const str = 'twas brillig in the slithy toves...';
-    const result = capitalizer(str);
-    expect(result).toBe('TWAS BRILLIG IN THE SLITHY TOVES...');
+jest.mock('../file-write', () => {
+  return () => Promise.resolve('Some more mock text');
+});
+
+describe('file reader test', () => {
+  const fileRead = require('../file-read');
+  const fileWrite = require('../file-write');
+
+  it('reads a file', () => {
+    const source = './text.txt';
+
+    return fileRead(source)
+      .then(contents => {
+        expect(contents).toEqual('Some mock text');
+      });
+  });
+
+  it('write a file', () => {
+    const source = './text.txt';
+
+    return fileWrite(source)
+      .then(contents => {
+        expect(contents).toEqual('Some more mock text');
+      });
   });
 });
